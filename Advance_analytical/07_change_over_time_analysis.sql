3a2b5c8c446200dbc2d7d8552d883127c5e6b1d8
Brahim Badreddine <83032475+BrahimBadreddine@users.noreply.github.com>
2026-04-24 23:17:14 +0100

    Add files via upload

---
+++ Advance_analytical/07_change_over_time_analysis.sql
@@ -0,0 +1,49 @@
+/*
+===============================================================================
+Step 7: Change Over Time Analysis
+===============================================================================
+Purpose:
+    - To track trends, growth, and changes in key metrics over time.
+    - For time-series analysis and identifying seasonality.
+    - To measure growth or decline over specific periods.
+
+SQL Functions Used:
+    - Date Functions: DATEPART(), DATETRUNC(), FORMAT()
+    - Aggregate Functions: SUM(), COUNT(), AVG()
+===============================================================================
+*/
+
+-- Analyse sales performance over time
+-- Quick Date Functions
+SELECT 
+    YEAR(order_date) AS Order_Year,
+    MONTH(order_date) AS Order_Month,
+    SUM(sales_amount) AS Total_sales,
+    COUNT(DISTINCT customer_key) AS Total_Customers,
+    SUM(quantity) AS Total_quantity
+FROM gold.fact_sales
+WHERE order_date IS NOT NULL
+GROUP BY YEAR(order_date), MONTH(order_date)
+ORDER BY YEAR(order_date), MONTH(order_date)
+
+-- DATETRUNC()
+SELECT 
+    DATETRUNC(month, order_date) AS Order_Year,
+    SUM(sales_amount) AS Total_sales,
+    COUNT(DISTINCT customer_key) AS Total_Customers,
+    SUM(quantity) AS Total_quantity
+FROM gold.fact_sales
+WHERE order_date IS NOT NULL
+GROUP BY DATETRUNC(month, order_date)
+ORDER BY DATETRUNC(month, order_date)
+
+-- FORMAT(): Function output is a String so the data here is sorted by the year
+SELECT 
+    FORMAT(order_date, 'yyyy-MMM') AS Order_Year,
+    SUM(sales_amount) AS Total_sales,
+    COUNT(DISTINCT customer_key) AS Total_Customers,
+    SUM(quantity) AS Total_quantity
+FROM gold.fact_sales
+WHERE order_date IS NOT NULL
+GROUP BY FORMAT(order_date, 'yyyy-MMM')
+ORDER BY FORMAT(order_date, 'yyyy-MMM')
